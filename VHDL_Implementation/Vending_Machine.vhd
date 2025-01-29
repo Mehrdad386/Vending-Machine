858628ce51b48bde55660dae392a7f90284bf08e
Mehrdad <mehrdadmrs10@gmail.com>
2025-01-29 18:13:16 +0330

    basic VHD file added

---
+++ VHDL_Implementation/Vending_Machine.vhd
@@ -0,0 +1,12 @@
+LIBRARY library IEEE;
+USE IEEE.std_logic_1164.ALL;
+USE IEEE.numeric_std.ALL;
+
+ENTITY Vending_Machine IS
+    PORT ();
+END Vending_Machine;
+
+ARCHITECTURE Behavioral OF Vending_Machine IS
+
+BEGIN
+END Behavioral;
