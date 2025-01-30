cb412f7337562a448d606f5388072a28e930babf
Mehrdad <mehrdadmrs10@gmail.com>
2025-01-30 19:07:49 +0330

    some small fixes

--- VHDL_Implementation/Coin_Input_Controller.vhd
+++ VHDL_Implementation/Coin_Input_Controller.vhd
@@ -1,31 +1,34 @@
-LIBRARY IEEE;
-USE IEEE.std_logic_1164.ALL;
-USE IEEE.numeric_std.ALL;
-ENTITY Coin_Input_Controller IS
-    PORT (
-        clk : IN STD_LOGIC;
-        reset : IN STD_LOGIC;
-        coin_input : IN STD_LOGIC_VECTOR (1 DOWNTO 0); --(1,5,10)
-        balance : OUT INTEGER
-    );
+LIBRARY IEEE;  
+USE IEEE.std_logic_1164.ALL;  
+USE IEEE.numeric_std.ALL;  
 
-END Coin_Input_Controller;
-ARCHITECTURE Behavioral OF Coin_Input_Controller IS
-    SIGNAL internal_balance : INTEGER := 0;
-BEGIN
-    PROCESS (clk, reset)
-    BEGIN
-        IF reset = '1' THEN
-            internal_balance <= 0;
-        ELSIF rising_edge(clk) THEN
-            CASE Coin_Input IS
-                WHEN "01" => internal_balance <= internal_balance + 1;
-                WHEN "10" => internal_balance <= internal_balance + 5;
-                WHEN "11" => internal_balance <= internal_balance + 10;
-                WHEN OTHERS => NULL;
-            END CASE;
-        END IF;
-        balance <= internal_balance;
-    END PROCESS;
+ENTITY Coin_Input_Controller IS  
+    PORT (  
+        clk : IN STD_LOGIC;  
+        reset : IN STD_LOGIC;  
+        coin_input : IN STD_LOGIC_VECTOR (1 DOWNTO 0); --(1, 5, 10)  
+        balance : OUT INTEGER  
+    );  
+END Coin_Input_Controller;  
+
+ARCHITECTURE Behavioral OF Coin_Input_Controller IS  
+    SIGNAL internal_balance : INTEGER := 0;  
+BEGIN  
+    PROCESS (clk, reset)  
+    BEGIN  
+        IF reset = '1' THEN  
+            internal_balance <= 0;  
+        ELSIF rising_edge(clk) THEN  
+            CASE coin_input IS  
+                WHEN "01" => internal_balance <= internal_balance + 1;  
+                WHEN "10" => internal_balance <= internal_balance + 5;  
+                WHEN "11" => internal_balance <= internal_balance + 10;  
+                WHEN OTHERS => NULL;  
+            END CASE;  
+        END IF;  
+    END PROCESS;  
+
+    -- Assign internal_balance to balance output  
+    balance <= internal_balance;  
 
 END Behavioral;
--- VHDL_Implementation/Item_selector.vhd
+++ VHDL_Implementation/Item_selector.vhd
@@ -1,34 +1,34 @@
-LIBRARY IEEE;
-USE IEEE.std_logic_1164.ALL;
-USE IEEE.numeric_std.ALL;
-ENTITY Item_selector IS
+LIBRARY IEEE;  
+USE IEEE.std_logic_1164.ALL;  
+USE IEEE.numeric_std.ALL;  
 
-    PORT (
-        clk : IN STD_LOGIC;
-        reset : IN STD_LOGIC;
-        item_selection : IN STD_LOGIC_VECTOR (1 DOWNTO 0);
-        item_available : IN STD_LOGIC;
-        item_price : OUT INTEGER
-    );
-END Item_selector;
-ARCHITECTURE Behavioral OF Item_Selector IS
+ENTITY Item_Selector IS  
+    PORT (  
+        clk : IN STD_LOGIC;  
+        reset : IN STD_LOGIC;  
+        item_selection : IN STD_LOGIC_VECTOR (1 DOWNTO 0);  
+        item_available : IN STD_LOGIC;  
+        item_price : OUT INTEGER  
+    );  
+END Item_Selector;  
 
-BEGIN
-    PROCESS (reset, clk)
-    BEGIN
-        IF reset = '1' THEN
-            item_price <= 0;
-        ELSIF rising_edge(clk) THEN
-            IF item_available = '1' THEN
-                CASE item_selection IS
-                    WHEN "00" => item_price <= 5; -- item A
-                    WHEN "01" => item_price <= 7; -- item B
-                    WHEN "10" => item_price <= 10; -- item C
-                    WHEN OTHERS => item_price <= 0; -- invalid selection
-                END CASE;
-            ELSE
-                item_price <= 0;
-            END IF;
-        END IF;
-    END PROCESS;
+ARCHITECTURE Behavioral OF Item_Selector IS  
+BEGIN  
+    PROCESS (reset, clk)  
+    BEGIN  
+        IF reset = '1' THEN  
+            item_price <= 0;  
+        ELSIF rising_edge(clk) THEN  
+            IF item_available = '1' THEN  
+                CASE item_selection IS  
+                    WHEN "00" => item_price <= 5;   -- item A  
+                    WHEN "01" => item_price <= 7;   -- item B  
+                    WHEN "10" => item_price <= 10;  -- item C  
+                    WHEN OTHERS => item_price <= 0; -- invalid selection  
+                END CASE;  
+            ELSE  
+                item_price <= 0;  
+            END IF;  
+        END IF;  
+    END PROCESS;  
 END Behavioral;
